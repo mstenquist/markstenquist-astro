db25e0dc4ac4bb399834ebf82ad6661828c1d272
Mark Stenquist <mark@stenquist.com>
2025-04-09 10:26:52 -0400

    chore: update copywriting a it

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Mark Stenquist | Frontend Engineer & AI Explorer';
-export const SITE_DESCRIPTION = 'Frontend engineer with 15+ years experience, exploring the intersection of web development and AI to create innovative digital experiences.';
+export const SITE_TITLE = 'Mark Stenquist';
+export const SITE_DESCRIPTION = 'Frontend engineer with 15+ years experience, currently exploring the intersection of web development and AI to create innovative digital experiences.';
